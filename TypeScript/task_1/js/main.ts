7498f7806bff835e598d6e12614f9c467e47bb3a
VCadenasu <vlcadenasu@gmail.com>
2024-06-08 23:51:48 -0500

    ADD: add class and methods in task_1

--- TypeScript/task_1/js/main.ts
+++ TypeScript/task_1/js/main.ts
@@ -10,3 +10,37 @@ interface Teacher {
 interface Directors extends Teacher {
     numberOfReports: number;
 }
+
+function printTeacher(firstName: string, lastName: string) {
+    const firstLetter = firstName.charAt(0);
+    return `${firstLetter}. ${lastName}`;
+}
+
+interface printTeacherFunction {
+    name: string;
+}
+
+interface StudentConstructorArgs {
+    firstName: string,
+    lastName: string,
+}
+
+interface Student {
+    workOnHomework(): string;
+    displayName(): string;
+}
+
+class StudentClass implements Student {
+    firstName: string;
+    lastName: string;
+    constructor(args: StudentConstructorArgs) {
+        this.firstName = args.firstName;
+        this.lastName = args.lastName;
+    }
+    workOnHomework(): string {
+        return 'Currently working';
+    }
+    displayName(): string {
+        return this.firstName;
+    }
+}
