e52a45e906f85c48107f82fee38083390a59e4ab
VCadenasu <vlcadenasu@gmail.com>
2024-06-11 21:25:52 -0500

    ADD: Directory task_1

---
+++ Webpack/task_1/js/dashboard_main.js
@@ -0,0 +1,17 @@
+import $ from "jquery";
+
+let count = 0;
+function updateCounter() {
+    count++,
+    $('count').text(`${count} clicks on the button`);
+}
+
+$('body').append('<p>Holberton Dashboard</p>');
+$('body').append('<p>Dashboard data for the students</p>');
+$('body').append('<button type="Button">Click here to get started</button>');
+$('body').append('<p id="count"></p>');
+$('body').append('<p>Copyright - Holberton School</p>')
+
+const debouncedUpdate = _.debounce(updateCounter, 500);
+
+$('button').on('click', debouncedUpdate);
